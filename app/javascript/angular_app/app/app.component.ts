c728e145747a0a68de4d622c9b3042433e4a98a5
Akira Nakazawa <nakazawa.akira@gmail.com>
2017-05-20 20:10:35 +0900

    4th commit

--- app/javascript/angular_app/app/app.component.ts
+++ app/javascript/angular_app/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hello-angular',
-  template: `<h1>Hello {{name}}</h1>`
+  selector: 'angular-app',
+  template: `Hello world!`
 })
 export class AppComponent {
-  name = 'Angular!';
 }
