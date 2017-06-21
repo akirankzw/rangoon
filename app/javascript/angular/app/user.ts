89357ffa120e0e4db52c5160c1eac09c7906582d
Akira Nakazawa <nakazawa.akira@gmail.com>
2017-06-21 13:41:29 +0900

    set default parameters for user.ts

--- app/javascript/angular/app/user.ts
+++ app/javascript/angular/app/user.ts
@@ -1,12 +1,12 @@
 export class User {
   constructor(
-    public id: number,
-    public email: string,
-    public given_name: string,
-    public family_name: string,
-    public password: string,
-    public password_confirmation: string,
-    public birthdate: string,
-    public gender: string
+    public id: number = null,
+    public email: string = '',
+    public given_name: string = '',
+    public family_name: string = '',
+    public password: string = '',
+    public password_confirmation: string = '',
+    public birthdate: string = '',
+    public gender: string = ''
   ) { }
 }
--- app/javascript/angular/app/user/user-signup.component.ts
+++ app/javascript/angular/app/user/user-signup.component.ts
@@ -10,17 +10,13 @@ import templateString from './user-signup.component.html';
 export class UserSignupComponent {
   submitted = false;
 
-  model = new User(0, 'test@example.com', 'Taro', 'Suzuki', '', '', '', ''); // TODO
+  model: User = new User();
 
   onSubmit() {
     this.submitted = true;
     // authenticity_token
     document.forms[1].appendChild(document.forms[0].elements[1]);
     document.forms[1].submit();
-  }
-
-  newUser() {
-    this.model = new User(0, '', '', '', '', '', '', '');
   }
 
   get diagnostic() { return JSON.stringify(this.model); };
