bf00e7b98a63d8599f9fa4d8fd9c59f096c1494a
Akira Nakazawa <nakazawa.akira@gmail.com>
2017-06-30 08:57:34 +0900

    change layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   around_action :set_time_zone, if: :operator
+  rescue_from ActionController::UnknownFormat, with: :redirect_to_dashboard
 
   private
 
@@ -10,5 +11,10 @@ class ApplicationController < ActionController::Base
 
   def set_time_zone
     Time.use_zone(operator.timezone) { yield }
+  end
+
+  def redirect_to_dashboard
+    redirect_to dashboard_admin_teachers_url if teacher_signed_in?
+    redirect_to dashboard_users_url
   end
 end
--- app/javascript/angular/app/app.component.ts
+++ app/javascript/angular/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'angular',
-  template: `
-    <router-outlet></router-outlet>
-    `
+  template: `<router-outlet></router-outlet>`
 })
 
 export class AppComponent {}
